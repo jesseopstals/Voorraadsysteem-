ac3ddd51ddf6557a76cf4e499c4669dd28982980
Jesse Opstals <91732321+jesseopstals@users.noreply.github.com>
2023-01-18 20:04:29 +0100

    Searchbar

---
+++ Voorraad.js
@@ -0,0 +1,16 @@
+document.querySelector("#typebar")
+addEventListener("input", filterlist)
+
+function filterlist(){
+    const searchInput = document.querySelector
+    ('#typebar')
+    const filter = searchInput.value.toLowerCase()
+    const listItems = document.querySelectorAll('.stock-item')
+
+    listItems.forEach((item) =>{
+        let text = item.textContent
+        if(text.toLowerCase().includes(filter.tolowerCase())){
+            
+        }
+    })
+}
