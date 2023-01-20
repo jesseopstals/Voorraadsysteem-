f6c36118502b66a3a5067d4c5786082e63377fb9
jesseopstals <jesseopstals@gmail.com>
2023-01-20 13:33:32 +0100

    display data

--- Voorraad.js
+++ Voorraad.js
@@ -1,4 +1,7 @@
-document.querySelector("#typebar")
+let dishlist = document.querySelector('.dishlist');
+let stockitem = document.querySelector('.stockitem');
+
+document.querySelector("#typebar");
 addEventListener("input", filterlist);
 
 function filterlist(){
@@ -16,3 +19,31 @@ function filterlist(){
         }
     });
 }
+
+function displayDishes(type) {
+    $.ajax({
+        type: "GET",
+        url: `https://localhost:7269/api/Dish/dishtype/${type}`,
+        encode: false,
+    }).done(function (data) {
+        loadDishHTML(data)
+    });
+}
+
+function loadDishHTML(data) {
+    dishlist.innerHTML = "";
+    for (let x = 0; x < data.length; x++) {
+        let dish = data[x]
+
+        let stocklist = document.createElement('li');
+        stocklist.className = "stockitem";
+        stocklist.innerHTML = `
+        <button class="stockitem">
+            <p class="stock-name">${dish.name}</p>
+            <p class="stock-amount">${dish.stockAmount}</p>
+            <p class="stock-date"></p>
+        </button>`
+    }}
+
+    let stocklist = document.createElement('li');
+    dishlist.appendChild(stocklist);
